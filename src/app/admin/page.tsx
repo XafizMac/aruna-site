6a7dc388cb156f6cf1d199fc67220dabcdbb784e
Жолдошов Абдулхафиз <guest1@MacBook-Pro-shama.local>
2024-08-15 00:20:50 +0600

    first-second commit

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Button } from "@geist-ui/core";
+import { FC } from "react";
+
+const Admin: FC = () => {
+    return(
+        <Button placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Admin page</Button>
+    )
+}
+
+export default Admin;
---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1df3vKc1c6a3MeuvhkjnYCvL2fvKi5Ng",
+  authDomain: "aruna-d70f4.firebaseapp.com",
+  projectId: "aruna-d70f4",
+  storageBucket: "aruna-d70f4.appspot.com",
+  messagingSenderId: "630556434295",
+  appId: "1:630556434295:web:d756be29fd0aa65bebcb72",
+  measurementId: "G-S2R9QYJVDR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
